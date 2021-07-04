f86a90f6e84a0e125d073d33400d022a956f085a
archismansaha <archimansaha@gmail.com>
2021-07-04 12:39:51 +0530

    all

--- DAY 1/ball.py
+++ DAY 1/ball.py
@@ -3,28 +3,29 @@ from turtle import Turtle
 
 class Ball(Turtle):
 
-    def __int__(self):
-
+    def __init__(self):
         super().__init__()
+        self.color("white")
         self.shape("circle")
-        self.color("black")
         self.penup()
-        self.x_move =10
-        self.y_move=10
+        self.x_move = 3
+        self.y_move = 3
+        self.move_speed = 0.1
 
     def move(self):
-        new_x =self.xcor()+self.x_move
-        new_y = self.ycor()+self.y_move
-        self.goto(new_x,new_y)
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
 
     def bounce_y(self):
-        self.y_move= -1 * self.y_move
-        
+        self.y_move *= -1
 
     def bounce_x(self):
-        self.x_move= -1 * self.x_move
+        self.x_move *= -1
+        self.move_speed *= 0.9
 
     def reset_position(self):
-        self.goto(0,0)
+        self.goto(0, 0)
+        self.move_speed = 0.1
         self.bounce_x()
-                
+
